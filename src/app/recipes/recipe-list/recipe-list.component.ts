8b7cde54932e96335b17c54d58f47926d6aa8f45
SHARMA Abhishek <abhishek.sharma3@soprasteria.com>
2018-03-16 15:29:30 +0530

    item setup 2

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,8 +8,9 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
 
-  recipe : Recipe[] = [
+  recipes : Recipe[] = [
     new Recipe('test recipe' , 'This is simply a test' ,  'http://maxpixel.freegreatpicture.com/static/photo/1x/Meat-Power-Recipe-Food-Dishes-Pork-1459693.jpg')
+    ,new Recipe('test recipe' , 'This is simply a test' ,  'http://maxpixel.freegreatpicture.com/static/photo/1x/Meat-Power-Recipe-Food-Dishes-Pork-1459693.jpg')
   ];
 
   constructor() { }
