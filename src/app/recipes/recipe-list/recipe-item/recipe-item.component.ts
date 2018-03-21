aeea486080dd61b295bf8ac0943dc5c7c2cbbc68
SHARMA Abhishek <abhishek.sharma3@soprasteria.com>
2018-03-21 15:45:16 +0530

    recipe detail using routes

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -11,16 +11,9 @@ import { RecipeService } from '../../recipe.service';
 export class RecipeItemComponent implements OnInit {
 
   @Input()
-  recipe : Recipe; 
-
-  constructor( private recipeService : RecipeService ) { }
+  recipe : Recipe;
 
   ngOnInit() {
-  }
-
-  onSelected()
-  {
-    this.recipeService.recipeSelected.emit(this.recipe);
   }
 
 }
