5cfbc0180549866392e25e149aacf928fc1d0e0e
SHARMA Abhishek <abhishek.sharma3@soprasteria.com>
2018-03-21 16:09:12 +0530

    edit routes established

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
 import {  } from 'events';
 import { RecipeService } from '../recipe.service';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-list',
@@ -15,10 +16,14 @@ export class RecipeListComponent implements OnInit {
 
   recipes : Recipe[] ;
 
-  constructor( private recipeService : RecipeService) { }
+  constructor( private recipeService : RecipeService , private router : Router , private route : ActivatedRoute) { }
 
   ngOnInit() {
     this.recipes = this.recipeService.getRecipes();
+  }
+
+  onNewRecipe(){
+    this.router.navigate(['new'], {relativeTo : this.route});
   }
 
 }
