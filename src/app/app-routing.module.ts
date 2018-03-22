2521c2aeb05711b4655ecbcd2eeb10f97c1b129a
SHARMA Abhishek <abhishek.sharma3@soprasteria.com>
2018-03-22 16:56:45 +0530

    lazy loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,9 +4,11 @@ import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
 import { SignupComponent } from "./auth/signup/signup.component";
 import { SigninComponent } from "./auth/signin/signin.component";
 import { AuthGuard } from "./auth/auth-guard.service";
+import { HomeComponent } from "./home/home.component";
 
 const appRoutes : Routes =  [
-    { path : '' , redirectTo : '/recipes' , pathMatch : 'full' },
+    { path : ''  , pathMatch : 'full' , component : HomeComponent },
+    { path : 'recipes' , loadChildren : './recipes/recipe.module#RecipeModule'  },
     { path : 'shopping-list' , component : ShoppingListComponent }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,23 +11,23 @@ import { RecipeService } from './recipes/recipe.service';
 import { DataStorageService } from './shared/data-storage.service';
 import { AuthService } from './auth/auth.service';
 import { AuthGuard } from './auth/auth-guard.service';
-import { RecipeModule } from './recipes/recipe.module';
 import { SharedModule } from './shared/shared.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { FormsModule } from '@angular/forms';
 import { AuthModule } from './auth/auth.module';
+import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpModule,
-    RecipeModule,
     ShoppingListModule,
     SharedModule,
     FormsModule,
