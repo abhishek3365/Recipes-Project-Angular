9ec368ed2ef3d300483f960e72ec37454bedbe2e
SHARMA Abhishek <abhishek.sharma3@soprasteria.com>
2018-03-22 12:29:08 +0530

    firebase setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
   loadedFeature : string = 'recipe';
 
   onNavigate( feature : string )
   {
     this.loadedFeature = feature
+  }
+
+  ngOnInit(){
+    firebase.initializeApp({
+      apiKey: "AIzaSyCgje-VV8OdK_t-fNPzuqGW8F9bSvMeMqQ",
+      authDomain: "udemy-project-d560b.firebaseapp.com"
+    });
   }
 
 }
---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class AuthService {
+
+  constructor() { }
+
+  signUp( email : string , password : string ){
+
+  }
+
+}
