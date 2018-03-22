f3488748388ae7a34b62305317d0c85f009227eb
SHARMA Abhishek <abhishek.sharma3@soprasteria.com>
2018-03-22 14:45:43 +0530

    log out

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Output , EventEmitter} from '@angular/core';
 import { DataStorageService } from '../shared/data-storage.service';
 import { Response } from '@angular/http';
+import { AuthService } from '../auth/auth.service';
 
 @Component({
   selector: 'app-header',
@@ -9,7 +10,7 @@ import { Response } from '@angular/http';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor( private dsService : DataStorageService ) { }
+  constructor( private dsService : DataStorageService , private authService : AuthService ) { }
 
   ngOnInit() {
   }
@@ -22,6 +23,10 @@ export class HeaderComponent implements OnInit {
 
   onFetchData() {
     this.dsService.getRecipes();
+  }
+
+  onLogout(){
+    this.authService.logout();
   }
 
 }
